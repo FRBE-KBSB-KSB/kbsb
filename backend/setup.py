050cbd01493116430a3693bea04e2291352e8041
Ruben Decrop <ruben@decrop.net>
2019-05-08 18:26:29 +0200

    updated python setup

--- backend/setup.py
+++ backend/setup.py
@@ -1,25 +1,4 @@
-import os
+# setup.py
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-requires = []
-
-setup(
-    name='kbsb',
-    version='0.1.0',
-    description='Royal Belgian Chess Federatin',
-    long_description='',
-    classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Django-cms",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-    ],
-    author='Ruben Decrop',
-    author_email='ruben@decrop.net',
-    url='https://www.frbe-kbsb.be',
-    keywords='Chess, Belgium',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-)
+setup(setup_requires=["pbr"], pbr=True)
