ef4fd2fa2d4c02785f64b040d0b5972193cf88f2
Ruben Decrop <ruben@decrop.net>
2019-05-09 20:28:04 +0200

    move back to setup.py, adding setuptools-git-version

--- backend/setup.py
+++ backend/setup.py
@@ -1,4 +1,24 @@
-# setup.py
-from setuptools import setup
+import os
 
-setup(setup_requires=["pbr"], pbr=True)
+from setuptools import setup, find_packages
+
+setup(
+    name='kbsb',
+    version_format='{tag}.dev{commitcount}+{gitsha}',
+    setup_requires=['setuptools-git-version'],
+    description='Royal Belgian Chess Federatin',
+    long_description='',
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django-cms",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='Ruben Decrop',
+    author_email='ruben@decrop.net',
+    url='https://www.frbe-kbsb.be',
+    keywords='Chess, Belgium',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
