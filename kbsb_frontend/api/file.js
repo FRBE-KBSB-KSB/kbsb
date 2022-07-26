d719bd1c755d93a62fc790b5cb6fcd11840692da
Ruben Decrop <ruben@chessdevil.net>
2022-07-26 21:59:49 +0200

    api_file working again reddevil 2.6.1 calndar geraardsbergen

--- kbsb_frontend/api/file.js
+++ kbsb_frontend/api/file.js
@@ -1,49 +1,49 @@
 export default context => ({
-  anon_get_files (options) {
+  anon_get_files(options) {
     const { reports, ...options1 } = options
     console.log('inside anon_get_files', reports, options1)
-    return context.$axios.get('/api/a/files', { params: { reports } })
+    return context.$axios.get('/api/v1/a/files', { params: { reports } })
   },
-  async add_file (options) {
+  async add_file(options) {
     const { token, ...options1 } = options
     console.log('add file', options1, token)
-    const resp = await context.$axios.post('/api/files', options1, {
+    const resp = await context.$axios.post('/api/v1/files', options1, {
       headers: {
         Authorization: 'Bearer ' + token
       }
     })
     return resp
   },
-  async delete_file (options) {
+  async delete_file(options) {
     const { id, token } = options
-    const resp = await context.$axios.delete(`/api/file/${id}`, {
+    const resp = await context.$axios.delete(`/api/v1/file/${id}`, {
       headers: {
         Authorization: 'Bearer ' + token
       }
     })
     return resp
   },
-  async get_file (options) {
+  async get_file(options) {
     const { id, token } = options
-    const resp = await context.$axios.get(`/api/file/${id}`, {
+    const resp = await context.$axios.get(`/api/v1/file/${id}`, {
       headers: {
         Authorization: 'Bearer ' + token
       }
     })
     return resp
   },
-  async get_files (options) {
+  async get_files(options) {
     const { token } = options
-    const resp = await context.$axios.get('/api/files', {
+    const resp = await context.$axios.get('/api/v1/files', {
       headers: {
         Authorization: 'Bearer ' + token
       }
     })
     return resp
   },
-  async update_file (options) {
+  async update_file(options) {
     const { id, token, ...options1 } = options
-    const resp = await context.$axios.put(`/api/file/${id}`, options1, {
+    const resp = await context.$axios.put(`/api/v1/file/${id}`, options1, {
       headers: {
         Authorization: 'Bearer ' + token
       }
