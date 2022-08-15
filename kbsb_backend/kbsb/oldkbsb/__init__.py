967b02b156cfd4fddee53e632c5a5761bb3ee4c1
Ruben Decrop <ruben@chessdevil.net>
2022-08-15 09:36:26 +0200

    redo changes

--- kbsb_backend/kbsb/oldkbsb/__init__.py
+++ kbsb_backend/kbsb/oldkbsb/__init__.py
@@ -4,12 +4,14 @@
 # these section contains the code to interact with the old mysqldb
 
 from .md_old import (
+    OldClub_sql,
     OldLoginValidator,
     OldMember,
     OldMember_sql,
     OldMemberList,
     OldUser,
     OldUser_sql,
+    old_role_mapping,
 )
 
 from .old import (
