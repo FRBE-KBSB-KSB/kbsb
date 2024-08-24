510841023c006ced86cb2e5a93922bbb972a322f
Ruben Decrop <ruben@chessdevil.net>
2024-08-24 18:38:52 +0200

    fix generate error in index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>KBSB FRBE KSB</title>
-  </head>
-
-  <body>
-    <h4>Redirecting ...</h4>
-    <script>
-      setTimeout(function () {
-        window.location.href = "https://blog.frbe-kbsb-ksb.be";
-      }, 60);
-    </script>
-  </body>
-</html>
+<script setup>
+setTimeout(function () {
+  window.location.href = "https://blog.frbe-kbsb-ksb.be";
+}, 60);
+</script>
+<template>
+  <h4>Redirecting ...</h4>
+</template>
