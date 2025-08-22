16ac389f943269ffe62d2127f295c4e9605385cd
Ruben Decrop <ruben@chessdevil.net>
2025-08-22 21:18:10 +0200

    added env_localtest provision 2526 for icseries

---
+++ backend/env_localtest.py
@@ -0,0 +1,102 @@
+# EMAIL = {
+#     "backend": "GMAIL",
+#     "serviceaccountfile": "kbsb-gmail.json",
+#     "sender": "ruben.decrop@frbe-kbsb-ksb.be",
+#     "account": "ruben.decrop@frbe-kbsb-ksb.be",
+#     "blindcopy": "",
+# }
+
+EMAIL = {
+    "backend": "SMTP",
+    "host": "schaken.decrop.net",
+    "port": "1025",
+    "sender": "ruben.decrop@frbe-kbsb-ksb.be",
+}
+
+FILESTORE = {
+    "manager": "google",
+    "bucket": "website-kbsb-prod.appspot.com",
+}
+
+ICDATA = "local"
+
+SECRETS = {
+    "mongodb": {
+        "name": "kbsb-mongodb-local",
+        "manager": "filejson",
+    },
+    "mysql": {
+        "name": "kbsb-mysql-infomaniak",
+        "manager": "filejson",
+    },
+    "gmail": {
+        "name": "kbsb-gmail",
+        "manager": "filejson",
+    },
+    "statamic": {
+        "name": "statamic-server",
+        "manager": "filejson",
+    },
+    "known-hosts": {
+        "name": "known-hosts",
+        "manager": "filejson",
+    },
+    "S_001": {
+        "name": "S_001",
+        "manager": "filejson",
+    },
+}
+
+SHORTCUT_INFOMANIAKLOGIN = True
+
+LOG_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s: %(asctime)s - %(name)s - %(message)s",
+        },
+        "color": {
+            "format": "%(log_color)s%(levelname)s%(reset)s: %(asctime)s %(bold)s%(name)s%(reset)s %(message)s",
+            "()": "reddevil.core.colorlogfactory.c_factory",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "color",
+            "stream": "ext://sys.stderr",
+        }
+    },
+    "loggers": {
+        "kbsb": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "reddevil": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "fastapi": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "uvicorn": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
+TEMPLATES_PATH = "./backend/kbsb/templates"
+
+TOKEN = {
+    "timeout": 180,  # timeout in minutes
+    "secret": "Pakjezakjemaggoan,jangtvierkantmeklootnuut",
+    "algorithm": "HS256",
+    "nocheck": False,
+}
---
+++ backend/kbsb/etl/create_icseries_2526.py
@@ -0,0 +1,53 @@
+import asyncio
+import aiofiles
+import aiocsv
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from reddevil.core import (
+    register_app,
+    connect_mongodb,
+    close_mongodb,
+    get_settings,
+)
+from dotenv import load_dotenv
+import logging
+from kbsb import ROOT_DIR
+from kbsb.interclubs.series import script_addteam_icseries
+
+app = FastAPI(
+    title="FRBE-KBSB-KSB",
+    description="Website Belgian Chess federation FRBE KBSB KSB",
+    version="0",
+)
+load_dotenv()
+register_app(app=app, settingsmodule="kbsb.settings")
+settings = get_settings()
+logger = logging.getLogger("kbsb")
+logger.info("Started")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    connect_mongodb()
+    yield
+    close_mongodb()
+
+
+async def main():
+    infile = ROOT_DIR / "shared" / "db" / "icseries2526.csv"
+    async with lifespan(app) as writer:
+        async with aiofiles.open(
+            infile, mode="r", encoding="utf-8", newline=""
+        ) as reader:
+            async for team in aiocsv.AsyncDictReader(reader):
+                await script_addteam_icseries(
+                    division=int(team.get("division")),
+                    index=team.get("index"),
+                    name=team.get("name"),
+                    pairingnumber=team.get("pairingnr"),
+                    idclub=team.get("idclub"),
+                )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
