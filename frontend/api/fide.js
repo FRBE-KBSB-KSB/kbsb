b108bf29bf14b20a8f1039eb8e31dd03f07fc2e9
Ruben Decrop <ruben.decrop@optimile.eu>
2026-06-09 21:38:15 +0200

    moved fide api to /api/v1/fide using axios as Javascript http client added logging at server side

---
+++ frontend/api/fide.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const prefix = "/api/v1/fide";
+export default {
+  formdata: async function() {
+    return await axios.get(`${prefix}/form-data`);
+  },
+  generate: async function(options){
+    const { locale, ...formdata} = options
+    return await axios.post(`${prefix}/generate?locale=${locale}`, formdata)
+  },
+};
