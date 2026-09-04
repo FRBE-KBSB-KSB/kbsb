0f125a3be35cbbf7eedc59938afd9541b7d74b9c
Ruben Decrop <ruben@chessdevil.net>
2026-09-04 18:24:08 +0200

    v3.8.4 remove uinused code (#96)

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -55,7 +55,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL || (process.env.NODE_ENV === 'production' ? '/' : 'http://127.0.0.1:8000/'),
-      statamicurl: process.env.STATAMIC_URL || 'http://127.0.0.1:8000/',
       repo_branch: 'master',
     },
   },
